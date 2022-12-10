c056093459e6cd54ef72437c9ddf7407783f4cdf
Georg <kubrakgeorg@gmail.com>
2022-12-10 14:16:34 +0500

    echo "# DZ.Java.2.1" >> README.md git init git add README.md git commit -m "first commit" git branch -M main git remote add origin git@github.com:GeorgKubrak/DZ.Java.2.1.git git push -u origin main

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        int ticket = 1700;
+        int ticket = 1900;
         int mile = ticket / 20;
         System.out.println(mile);
     }
